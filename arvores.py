4676329d17096a7192e61b264e8b5255264e8f8f
Argus Portal <argusportal@gmail.com>
2025-03-22 14:19:36 -0300

    teste

--- arvores.py
+++ arvores.py
@@ -10,6 +10,7 @@ n_normais = n_transacoes - n_fraudes
 fraudes_porc = n_fraudes / n_transacoes
 normais_porc = n_normais / n_transacoes
 
+
 print("Número de transações: ", n_transacoes)
 print("Número de fraudes: ", n_fraudes, "%.2f" %(fraudes_porc*100))
 print("Número de transações normais: ", n_normais, "%.2f" %(normais_porc*100))
